607af5dfa1172959ae36ec6520b2057e08127f25
Cewei Fu <fucewei@jancsitech.net>
2024-04-09 17:08:36 +0800

    test:  verifyMerkle.js supplement;

--- contract/DAP/verifyMerkle.js
+++ contract/DAP/verifyMerkle.js
@@ -1,35 +1,25 @@
-import { MerkleTree } from "merkletreejs";
-import keccak256 from "keccak256";
+import { MerkleTree } from 'merkletreejs';
+import keccak256 from 'keccak256';
 
-
+// 定义白名单地址
 const WhiteList = [
-    "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
-    "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",
-    "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db",
-]
+    '0x5B38Da6a701c568545dCfcB03FcB875f56beddC4',
+    '0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2',
+    '0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db',
+];
 
 const leaves = WhiteList.map((x) => keccak256(x));
-const merkleTree = new MerkleTree(leaves, keccak256);
-const rootHash ="0x"+ merkleTree.getRoot().toString("hex");
-
-
-const testAddress ="0x5B38Da6a701c568545dCfcB03FcB875f56beddC4";
-
-
+const merkleTree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+const rootHash = '0x' + merkleTree.getRoot().toString('hex');
+const testAddress = '0x5B38Da6a701c568545dCfcB03FcB875f56beddC4';
 const leaf = keccak256(testAddress);
-const proof = merkleTree.getProof(leaf);
-const proof32 = proof.map((x) => "0x" + x.data.toString("hex"));
+const proof = merkleTree.getHexProof(leaf);
+const isWhiteList = merkleTree.verify(proof, leaf, rootHash);
 
-const isWhiteList = merkleTree.verify(proof, leaf, rootHash)
+console.log('rootHash:: ', rootHash);
+console.log('proof:: ', proof);
+console.log('verify:: ', isWhiteList);
 
-console.log("rootHash:: ", rootHash)
-console.log("leaf:: ", proof32)
-console.log("verify:: ",isWhiteList)
-
-
-// rootHash::  0xa7b65d7def4b9ccb18e34164671ec799d2f09710d3d80826bfacb00dc46bbd74
-// leaf::  [
-//     '0x999bf57501565dbd2fdcea36efa2b9aef8340a8901e3459f4a4c926275d36cdb',
-//     '0x04a10bfd00977f54cc3450c9b25c9b3a502a089eba0097ba35fc33c4ea5fcb54'
-// ]
-// verify::  true
+// rootHash::  0x74f4666169faccda89a45d47ab1997a62f24c3cd534a01539db8f0e40d3eb8b1
+// proof::     ["0x999bf57501565dbd2fdcea36efa2b9aef8340a8901e3459f4a4c926275d36cdb","0x04a10bfd00977f54cc3450c9b25c9b3a502a089eba0097ba35fc33c4ea5fcb54"]
+// verify::    true
