690d492e43b3211a8453c887e204dc0f91561257
Cewei Fu <fucewei@jancsitech.net>
2024-04-19 17:09:48 +0800

    fix: dap_nft add merkleProof;

--- contract/DAP/dap_nft.sol
+++ contract/DAP/dap_nft.sol
@@ -4,10 +4,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-interface IPointsSystem {
-    function verifyAddressInWhitelist(address account, bytes32[] memory proof) external view returns (bool);
-}
 
 contract NFTContract is ERC721URIStorage, Ownable, ReentrancyGuard {
     uint256 public constant MINT_PRICE = 0.001 ether;
@@ -17,15 +15,16 @@ contract NFTContract is ERC721URIStorage, Ownable, ReentrancyGuard {
     uint256 public paidMintCount;
     uint256 public tokenId;
     uint256 public wlMintCount;
-    IPointsSystem pointsSystem;
+    bytes32 public distributionRoot;
+
 
     // VIP address can mint quantity
     mapping(address => int256) mintAccountMap;
 
     uint256 private _totalSupply; // Currently issued NFT, including NFT purchased through VIP and non-VIP channels
 
-    constructor(string memory name, string memory symbol, address pointsSystemAddress) ERC721(name, symbol) Ownable(msg.sender) {
-        pointsSystem = IPointsSystem(pointsSystemAddress);
+    constructor(string memory name, string memory symbol, bytes32 root) ERC721(name, symbol) Ownable(msg.sender) {
+        distributionRoot=root;
         _totalSupply = 8888;
         tokenId = 0;
         startTime = 0;
@@ -39,16 +38,23 @@ contract NFTContract is ERC721URIStorage, Ownable, ReentrancyGuard {
     function totalSupply() public view returns (uint256) {
         return _totalSupply;
     }
+    function setDistributionRoot(bytes32 root) public onlyOwner {
+        distributionRoot = root;
+    }
+    function verifyAddressInWhitelist(address account, bytes32[] memory proof) public view returns (bool) {
+        bytes32 encodedAccount = keccak256(abi.encodePacked(account));
+        return MerkleProof.verify(proof, distributionRoot, encodedAccount);
+    }
 
     function checkIfUserIsWhitelisted(address user, bytes32[] memory proof) public view returns (bool) {
-        return pointsSystem.verifyAddressInWhitelist(user, proof);
+        return verifyAddressInWhitelist(user, proof);
     }
 
     //    ["0x05Ab5025Fe3f0a0d26fcd5201448D0Ffb16370df", "0x121e33197fa468de29e294e66be1a72e89a8472e", "0x1b8F7EEfcf7773bf1E8682894c4caA8e7A8f404e", "0x52474d9D1df3cf8293e47bd64B146F61c4Db5f13", "0x529B39b21fe590618bac914797D0f8c08560961A", "0x6b26f3cedfe85ca25280c30200148da12e9c11a6", "0x7d25dbFfFFec79B9f41014B911AAB218e1bB47dB", "0x8668B818959a5fbc2687080F4f62D1D8699EF78c", "0x9e18c807F0b5793fc3c596048351F9A2D1D747E3", "0xb17Dc4c21961fDeDF65AAA634c8ee8586b7dd81e", "0xcfefc97d1d1b7bd172a0399d0c64d4f32bdad6ee"]
     //    uint256 amount = 2;         Actual can mint quantity = amount
     function setVipAddressLimit(address[] memory addresses, int256 amount) external onlyOwner {
         require(addresses.length > 0, "Addresses array is empty");
-        require(amount > 1, "Amount must be greater than or equal to 0");
+        require(amount > 1, "Amount must be greater than or equal to 1");
 
         for (uint256 i = 0; i < addresses.length; i++) {
             mintAccountMap[addresses[i]] = 1 - amount;
